e7cf6f3d417b3d90ae9c0995758616cc17a51910
Tr3vorihno <enricocervi6@gmail.com>
2025-11-04 13:50:53 +0100

    push iniziale ottavo esercizio

---
+++ 4_sett/Es8_IV_Sett/Cerchio.java
@@ -0,0 +1,67 @@
+public class Cerchio
+{
+   /*
+       Construttore della classe Cerchio: centro (x,y) e raggio
+   */
+   public Cerchio (double x, double y, double radius)
+   {
+	//... completare
+   }
+
+   /*
+       Verifica se questo cerchio e il cerchio c sono coincidenti, 
+       ovvero se hanno tutti i punti in comune
+   */
+
+   public boolean isCoincident(Cerchio c)
+   {
+	//... completare
+   }
+
+
+   /*
+       verifica se questo cerchio e' interno al cerchio c o se
+       il cerchio c e' interno a questo cerchio
+   */
+   public boolean isEncircled(Cerchio c)
+   {
+	//... completare
+   }
+
+   /*
+      verifica se questo cerchio e il cerchio c sono esterni, ovvero non
+      sono contenuti uno nell'altro e non hanno alcun punto in comune
+   */
+   public boolean isExternal(Cerchio c)
+   {
+	//... completare
+   }
+          
+
+
+   /*
+      verifica se questo cerchio e il cerchio c sono secanti, 
+      ovvero se hanno almeno un punto in comune.
+   */
+   public boolean isSecant(Cerchio c)
+   {
+	//... completare
+   }
+          
+
+
+   /*
+      Verifica se questo cerchio e il cerchio c sono tangenti,
+      ovvero se hanno un solo punto in comune
+   */
+   public boolean isTangential(Cerchio c)
+   {
+	//... completare
+   }
+
+
+
+  //... campi di esemplare... da completare
+  //...
+
+}
